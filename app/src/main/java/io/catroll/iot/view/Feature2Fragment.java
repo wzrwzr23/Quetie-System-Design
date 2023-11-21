93466f698ffebbc42bb7459af275d98c080d7cb8
wzrwzr23 <1004867@mymail.sutd.edu.sg>
2023-11-22 00:44:50 +0800

    feat: feature 2

--- app/src/main/java/io/catroll/iot/view/Feature2Fragment.java
+++ app/src/main/java/io/catroll/iot/view/Feature2Fragment.java
@@ -1,27 +1,88 @@
 package io.catroll.iot.view;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+
 import io.catroll.iot.R;
 
 public class Feature2Fragment extends Fragment {
-
+    private Button chooseDateTimeButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feature_2, container, false);
+        View view = inflater.inflate(R.layout.fragment_feature_2, container, false);
+        chooseDateTimeButton = view.findViewById(R.id.chooseDateTimeButton);
+        chooseDateTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimePicker();
+            }
+        });
+        return view;
+    }
+    private void showDateTimePicker() {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                requireContext(),
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, month);
+                        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                                requireContext(),
+                                new TimePickerDialog.OnTimeSetListener() {
+                                    @Override
+                                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                        calendar.set(Calendar.MINUTE, minute);
+                                        handleDateTimeSelection(calendar);
+                                    }
+                                },
+                                hour,
+                                minute,
+                                true
+
+                        );
+                        timePickerDialog.show();
+                    }
+                },
+                year,
+                month,
+                day
+        );
+        datePickerDialog.show();
     }
 
+    private void handleDateTimeSelection(Calendar selectedDateTime) {
+        String formattedDateTime = android.text.format.DateFormat.format("yyyy-MM-dd HH:mm", selectedDateTime).toString();
+        Toast.makeText(requireContext(), "Selected Date and Time: " + formattedDateTime, Toast.LENGTH_LONG).show();
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
